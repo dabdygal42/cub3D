06c0a01b5365a4e7eaacd9dc8f089765f2facba6
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-04-02 14:11:57 +0400

    t_data added + defined key codes

--- include/cub3D.h
+++ include/cub3D.h
@@ -3,14 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dabdygal <dabdygal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 15:57:14 by dabdygal          #+#    #+#             */
-/*   Updated: 2024/03/18 15:58:00 by dabdygal         ###   ########.fr       */
+/*   Updated: 2024/04/02 14:02:07 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
+
+# define WINDOW_WIDTH	1600
+# define WINDOW_HEIGHT	1200
+
+# define KEY_7		89	
+# define KEY_8		91
+# define KEY_UP		13
+# define KEY_DOWN	1
+# define KEY_LEFT	0
+# define KEY_RIGHT	2
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*win;
+	int		pix_bits;
+	int		l_bytes;
+	int		endi;
+	void	*img;
+	char	*buf;
+	int		buf_size;
+	int		width;
+	int		height;
+}	t_data;
 
 #endif
