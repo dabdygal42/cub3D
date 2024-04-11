57eb7453de9436d99cc8004fc919a8ad26328548
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-04-11 14:29:28 +0400

    code organization

--- src/hooks.c
+++ src/hooks.c
@@ -6,49 +6,17 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 17:19:38 by akeryan           #+#    #+#             */
-/*   Updated: 2024/04/11 10:41:58 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/04/11 14:27:30 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	key_handler(int key, t_data *d)
+static void	left_right_keys(int key, t_data *d)
 {
 	double	old_dir;
 	double	old_plane_x;
 
-	//printf("key num: %d\n", key);
-
-	if (key == ESC)
-		close_window();
-	if (key == KEY_W)
-	{
-		if (d->world_map[(int)(d->pos_x + d->dir_x * d->move_speed)][(int)d->pos_y] == 0)
-			d->pos_x += d->dir_x * d->move_speed;
-		if (d->world_map[(int)d->pos_x][(int)(d->pos_y + d->dir_y * d->move_speed)] == 0)
-			d->pos_y += d->dir_y * d->move_speed;
-	}
-	if (key == KEY_S)
-	{
-		if (d->world_map[(int)(d->pos_x - d->dir_x * d->move_speed)][(int)d->pos_y] == 0)
-			d->pos_x -= d->dir_x * d->move_speed;
-		if (d->world_map[(int)d->pos_x][(int)(d->pos_y - d->dir_y * d->move_speed)] == 0)
-			d->pos_y -= d->dir_y * d->move_speed;
-	}
-	if (key == KEY_D)
-	{
-		if (d->world_map[(int)(d->pos_x + d->plane_x * d->move_speed)][(int)d->pos_y] == 0)
-			d->pos_x += d->plane_x * d->move_speed;
-		if (d->world_map[(int)d->pos_x][(int)(d->pos_y + d->dir_y * d->move_speed)] == 0)
-			d->pos_y += d->plane_y * d->move_speed;
-	}
-	if (key == KEY_A)
-	{
-		if (d->world_map[(int)(d->pos_x - d->plane_x * d->move_speed)][(int)d->pos_y] == 0)
-			d->pos_x -= d->plane_x * d->move_speed;
-		if (d->world_map[(int)d->pos_x][(int)(d->pos_y - d->plane_y * d->move_speed)] == 0)
-			d->pos_y -= d->plane_y * d->move_speed;
-	}
 	if (key == KEY_LEFT_ARROW)
 	{
 		old_dir = d->dir_x;
@@ -67,8 +35,42 @@ int	key_handler(int key, t_data *d)
 		d->plane_x = d->plane_x * cos(-d->rot_speed) - d->plane_y * sin(-d->rot_speed);
 		d->plane_y = old_plane_x * sin(-d->rot_speed) + d->plane_y * cos(-d->rot_speed);
 	}
-	render(d);
-	return (0);
+}
+
+static void	w_s_keys(int key, t_data *d)
+{
+	if (key == KEY_W)
+	{
+		if (d->world_map[(int)(d->pos_x + d->dir_x * d->move_speed)][(int)d->pos_y] == 0)
+			d->pos_x += d->dir_x * d->move_speed;
+		if (d->world_map[(int)d->pos_x][(int)(d->pos_y + d->dir_y * d->move_speed)] == 0)
+			d->pos_y += d->dir_y * d->move_speed;
+	}
+	if (key == KEY_S)
+	{
+		if (d->world_map[(int)(d->pos_x - d->dir_x * d->move_speed)][(int)d->pos_y] == 0)
+			d->pos_x -= d->dir_x * d->move_speed;
+		if (d->world_map[(int)d->pos_x][(int)(d->pos_y - d->dir_y * d->move_speed)] == 0)
+			d->pos_y -= d->dir_y * d->move_speed;
+	}
+}
+
+static void	a_d_keys(int key, t_data *d)
+{
+	if (key == KEY_D)
+	{
+		if (d->world_map[(int)(d->pos_x + d->plane_x * d->move_speed)][(int)d->pos_y] == 0)
+			d->pos_x += d->plane_x * d->move_speed;
+		if (d->world_map[(int)d->pos_x][(int)(d->pos_y + d->plane_y * d->move_speed)] == 0)
+			d->pos_y += d->plane_y * d->move_speed;
+	}
+	if (key == KEY_A)
+	{
+		if (d->world_map[(int)(d->pos_x - d->plane_x * d->move_speed)][(int)d->pos_y] == 0)
+			d->pos_x -= d->plane_x * d->move_speed;
+		if (d->world_map[(int)d->pos_x][(int)(d->pos_y - d->plane_y * d->move_speed)] == 0)
+			d->pos_y -= d->plane_y * d->move_speed;
+	}
 }
 
 int	close_window(void)
@@ -76,3 +78,20 @@ int	close_window(void)
 	exit(0);
 	return (0);
 }
+
+int	key_handler(int key, t_data *d)
+{
+	
+	if (key == KEY_W || key == KEY_S)
+		w_s_keys(key, d);
+	if (key == KEY_A || key == KEY_D)
+		a_d_keys(key, d);
+	if (key == KEY_LEFT_ARROW || key == KEY_RIGHT_ARROW)
+		left_right_keys(key, d);
+	if (key == ESC)
+		close_window();
+	render(d);
+	return (0);
+}
+
+
